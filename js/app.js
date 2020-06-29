4640b625a1eb8fc3ee13c22afcb6f4004cdf5049
Francoveliz <francoleandroveliz@gmail.com>
2020-06-29 04:30:34 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+function shareShow() {
+  document.getElementById("main-bar").style.visibility = "hidden";
+  document.getElementById("main-bar").style.position = "absolute";
+  document.getElementById("share-bar").style.visibility = "visible";
+  document.getElementById("share-bar").style.position = "static";
+}
+
+function shareHide() {
+  document.getElementById("main-bar").style.visibility = "visible";
+  document.getElementById("main-bar").style.position = "static";
+  document.getElementById("share-bar").style.visibility = "hidden";
+  document.getElementById("share-bar").style.position = "absolute";
+}
