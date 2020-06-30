cbdc64d5807a0b877c1154eff6b367fd3c913e7b
Francoveliz <francoleandroveliz@gmail.com>
2020-06-30 18:31:07 -0300

    movil version completed

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
-function shareShow() {
-  document.getElementById("main-bar").style.visibility = "hidden";
-  document.getElementById("main-bar").style.position = "absolute";
+function show() {
+  document.getElementById("profile-bar").style.visibility = "hidden";
+  document.getElementById("profile-bar").style.position = "absolute";
   document.getElementById("share-bar").style.visibility = "visible";
   document.getElementById("share-bar").style.position = "static";
+  var share = document.getElementById("share-bar");
+  share.classList.toggle("fade-in-fwd");
+
 }
 
-function shareHide() {
-  document.getElementById("main-bar").style.visibility = "visible";
-  document.getElementById("main-bar").style.position = "static";
+function hide() {
+  document.getElementById("profile-bar").style.visibility = "visible";
+  document.getElementById("profile-bar").style.position = "static";
   document.getElementById("share-bar").style.visibility = "hidden";
   document.getElementById("share-bar").style.position = "absolute";
-}
+  var share = document.getElementById("share-bar");
+  share.classList.toggle("fade-in-fwd");
 
-function test(){
-
-alert("hi");
 }
