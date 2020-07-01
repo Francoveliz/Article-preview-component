aec42bb0aac29eed2deccc0bd28283d638cfa6bf
Francoveliz <francoleandroveliz@gmail.com>
2020-07-01 05:37:53 -0300

    only missing the triangle

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
-function show() {
-  document.getElementById("profile-bar").style.visibility = "hidden";
-  document.getElementById("profile-bar").style.position = "absolute";
-  document.getElementById("share-bar").style.visibility = "visible";
-  document.getElementById("share-bar").style.position = "static";
-  var share = document.getElementById("share-bar");
-  share.classList.toggle("fade-in-fwd");
+function shareBarAnimation() {
+  var shareBar = document.getElementById("share-bar").style.display
+  if (shareBar == "none") {
+    document.getElementById("share-bar").style.display = "flex";
+    var fade = document.getElementById("share-bar");
+    fade.classList.add("fade-in-fwd");
 
-}
-
-function hide() {
-  document.getElementById("profile-bar").style.visibility = "visible";
-  document.getElementById("profile-bar").style.position = "static";
-  document.getElementById("share-bar").style.visibility = "hidden";
-  document.getElementById("share-bar").style.position = "absolute";
-  var share = document.getElementById("share-bar");
-  share.classList.toggle("fade-in-fwd");
+  } else {
+    document.getElementById("share-bar").style.display = "none";
+    var fade = document.getElementById("share-bar");
+    fade.classList.remove("fade-in-fwd");
+  }
 
 }
